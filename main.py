8b8b2fb7eaf6faf87ec23ec4b5289a466ba7c25d
terryszhou <terryszhou@gmail.com>
2022-02-25 11:00:04 -0600

    Pandas dataframe used to generate POS glossary .csv file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from nltk.corpus import twitter_samples
-from nltk.tag import pos_tag_sents
+from nltk.tag import pos_tag_sents, pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.help import upenn_tagset
+import nltk.data
+import pandas
+
+# # TWITTER SAMPLE ANALYSIS - - - - - - - -
 
 # Renders each tweet as a string
 tweets = twitter_samples.strings("positive_tweets.json")
@@ -8,9 +14,10 @@ tweets = twitter_samples.strings("positive_tweets.json")
 tweets_tokens = twitter_samples.tokenized("positive_tweets.json")
 
 # Renders a list of tuples containing each token and its POS (Part-of-Speech)
-# Adjectives are marked JJ. Nouns are NN or NNS based on plurality.
+# Adjectives are marked JJ. Nouns are NN.
 tweets_tagged = pos_tag_sents(tweets_tokens)
 
+# Iterates through POS-tagged tweets and tots up nouns and adjectives.
 def count_POS():
   JJ_COUNT = 0
   NN_COUNT = 0
@@ -21,6 +28,34 @@ def count_POS():
         JJ_COUNT += 1
       elif tag == "NN":
         NN_COUNT += 1
-  return f"Total number of adjectives is {JJ_COUNT}.", f"Total number of nouns is {NN_COUNT}."
+  print(f"Total number of adjectives is {JJ_COUNT}.")
+  print(f"Total number of nouns is {NN_COUNT}.")
 
-print(count_POS())
+# count_POS()
+
+# # CREATED POS GLOSSARY .CSV FILE
+
+def create_POS_glossary():
+  tagdict = nltk.data.load('help/tagsets/upenn_tagset.pickle')
+  pos_list = []
+  def_list = []
+  ex_list = []
+  for item in list(tagdict.items()):
+    pos_list.append(item[0])
+    def_list.append(item[1][0])
+    ex_list.append(item[1][1])
+  d = {"POS": pos_list, "Definition": def_list, "Examples": ex_list}
+  df = pandas.DataFrame(data = d)
+  df.to_csv("pos_glossary.csv")
+
+create_POS_glossary()
+
+# # SAMPLE SENTENCE ANALYSIS - - - - - - - -
+
+sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+
+# word_tokenize: converts string into tokens.
+tokens = word_tokenize(sentence)
+
+# pos_tag: converts tokens into POS-tagged tuples
+tagged = pos_tag(tokens)
