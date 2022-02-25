221da8b4540529f6ec7834e2b70e34a85736c1c5
terryszhou <terryszhou@gmail.com>
2022-02-25 10:02:57 -0600

    POS counting function complete

--- main.py
+++ main.py
@@ -6,4 +6,21 @@ tweets = twitter_samples.strings("positive_tweets.json")
 
 # Renders each string into a list of parseable tokens
 tweets_tokens = twitter_samples.tokenized("positive_tweets.json")
-print(tweets)
+
+# Renders a list of tuples containing each token and its POS (Part-of-Speech)
+# Adjectives are marked JJ. Nouns are NN or NNS based on plurality.
+tweets_tagged = pos_tag_sents(tweets_tokens)
+
+def count_POS():
+  JJ_COUNT = 0
+  NN_COUNT = 0
+  for tweet in tweets_tagged:
+    for pair in tweet:
+      tag = pair[1]
+      if tag == "JJ":
+        JJ_COUNT += 1
+      elif tag == "NN":
+        NN_COUNT += 1
+  return f"Total number of adjectives is {JJ_COUNT}.", f"Total number of nouns is {NN_COUNT}."
+
+print(count_POS())
